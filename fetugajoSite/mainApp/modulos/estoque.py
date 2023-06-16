2db7da5e9015abe78e30b8bff05eae8b396de936
FELIPE LOURENCO BARCELLOS <e2110710@tiete.grad.inf.puc-rio.br>
2023-06-16 10:21:03 -0300

    fechando arq

--- fetugajoSite/mainApp/modulos/estoque.py
+++ fetugajoSite/mainApp/modulos/estoque.py
@@ -5,8 +5,9 @@ def buscaNoEstoque(path, nomeJogo):
 
     if nomeJogo in jogos:
         if (jogos[nomeJogo]['quantidade'] > 0):
+            f.close()
             return True
-
+    f.close()
     return False
 
 def adicionaEstoqueAluguel(path, nomeJogo, qnt):
@@ -21,5 +22,4 @@ def adicionaEstoqueAluguel(path, nomeJogo, qnt):
             #Se o jogo nao esta no estoque, adiciona ele
             jogos[nomeJogo] = {"quantidade": qnt, "valor": 10}
         json.dump(jogos, open(path, "w"), indent=4)
-
     return
