13bcc5374e3fc6371629a15691c243681ce21719
Tulio Martins <113527485+tulio240@users.noreply.github.com>
2023-06-25 02:55:18 -0300

    Update aluguel.py

---
+++ fetugajoSite/mainApp/modulos/aluguel.py
@@ -0,0 +1,29 @@
+import json
+import cliente.py
+import estoque.py
+import compra.py
+
+def alugaJogo(nomeJogo, nomeCliente):
+    if verificaCliente(nomeCliente):
+        if buscaNoEstoque(nomeJogo): #checa se o jogo esta disponivel p aluguel
+            path = "data/estoqueAluguel.json"
+            removeEstoqueAluguel(nomeJogo, 1)
+            f = open(path, "r")
+            jogos = json.load(f)
+
+            adicionaSaldo(jogos[nomeJogo]['valor'])
+            f.close() 
+            return 0
+            
+        compraJogo(nomeJogo)
+            
+        return 1
+    return 2
+
+def devolveJogo(nomeJogo):
+    path = "data/estoqueAluguel.json"
+    with open(path, "r") as f:
+        jogos = json.load(f)
+        atual = jogos[nomeJogo]['quantidade']
+        jogos[nomeJogo]['quantidade'] = atual + qnt
+    return
