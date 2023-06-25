2fa10a379107eed46f5873c7b626d20b44b68f41
Tulio Martins <113527485+tulio240@users.noreply.github.com>
2023-06-25 19:18:42 -0300

    Update aluguel.py

--- fetugajoSite/mainApp/modulos/aluguel.py
+++ fetugajoSite/mainApp/modulos/aluguel.py
@@ -3,23 +3,34 @@ import cliente.py
 import estoque.py
 import compra.py
 
-def alugaJogo(nomeJogo, nomeCliente):
-    if verificaCliente(nomeCliente):
-        if buscaNoEstoque(nomeJogo): #checa se o jogo esta disponivel p aluguel
-            path = "data/estoqueAluguel.json"
-            removeEstoqueAluguel(nomeJogo, 1)
-            f = open(path, "r")
-            jogos = json.load(f)
+def validaAluguel(f):
+    def valida(nomeJogo, nomeCliente):
+        if verificaCliente(nomeCliente):
+            return alugaJogo(nomeJogo, nomeCliente)
+        return 2
+    return valida
 
-            adicionaSaldo(jogos[nomeJogo]['valor'])
-            f.close() 
-            return 0
+@validaAluguel
+def alugaJogo(nomeJogo, nomeCliente):
+    if buscaNoEstoque(nomeJogo): #checa se o jogo esta disponivel p aluguel
+        path = "data/estoqueAluguel.json"
+        removeEstoqueAluguel(nomeJogo, 1)
+        f = open(path, "r")
+        jogos = json.load(f)
+    
+        adicionaSaldo(jogos[nomeJogo]['valor'])
+        f.close() 
+        return 0
             
-        compraJogo(nomeJogo)
+    compraJogo(nomeJogo)
             
-        return 1
-    return 2
+    return 1
 
 def devolveJogo(nomeJogo):
-    adicionaEstoqueAluguel(nomeJogo, 1)
+    path = "data/estoqueAluguel.json"
+    with open(path, "r") as f:
+        jogos = json.load(f)
+        atual = jogos[nomeJogo]['quantidade']
+        jogos[nomeJogo]['quantidade'] = atual + qnt
     return
+  
