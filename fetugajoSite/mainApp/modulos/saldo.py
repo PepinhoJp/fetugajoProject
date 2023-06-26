46877afff07ecc1aa1ab2e328738fcf8640940cb
Tulio Martins <113527485+tulio240@users.noreply.github.com>
2023-06-25 22:13:35 -0300

    Update saldo.py

--- fetugajoSite/mainApp/modulos/saldo.py
+++ fetugajoSite/mainApp/modulos/saldo.py
@@ -7,17 +7,23 @@ def verificaSaldo():
     return saldoEmpresa['SaldoEmpresa']
 
 def adicionaSaldo( valor):
+    if type(valor) != int:
+        return 1
     with open("data/saldo.json") as f:
         saldoEmpresa = json.load(f)
 
         atual = saldoEmpresa['SaldoEmpresa']
         saldoEmpresa['SaldoEmpresa'] = float(atual) + valor
         json.dump(saldoEmpresa, open("data/saldo.json", "w"), indent=4)
+    return 0
 
 def removeSaldo( valor):
+    if type(valor) != int:
+        return 1
     with open("data/saldo.json") as f:
         saldoEmpresa = json.load(f)
 
         atual = saldoEmpresa['SaldoEmpresa']
         saldoEmpresa['SaldoEmpresa'] = float(atual) - valor
         json.dump(saldoEmpresa, open("data/saldo.json", "w"), indent=4)
+    return 0
